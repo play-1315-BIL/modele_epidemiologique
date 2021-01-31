4b20db749d958f80312d36b3a5294551e7372380
Luca <luca.brunod.indrigo@gmail.com>
2021-01-31 11:07:12 +0100

    report

--- julia/deterministic_model.jl
+++ julia/deterministic_model.jl
@@ -1,3 +1,5 @@
+using LambertW
+
 mutable struct DeterministicModelState
     s::Float64
     i::Float64
@@ -44,6 +46,15 @@ function increment_time!(model::DeterministicModel)
     new_state = DeterministicModelState(new_s, new_i)
 
     model.state = new_state
+end
+
+function r_limit(beta::Float64, gamma::Float64)
+    limit = 0
+    R_0 = beta/gamma
+    if R_0 > 1
+        limit = 1 + 1/R_0 * lambertw(-R_0 * exp(-R_0))
+    end
+    return limit
 end
 
 
--- julia/main.jl
+++ julia/main.jl
@@ -1,71 +1,75 @@
 include("markovian_model.jl")
 include("deterministic_model.jl")
 using Plots
-using LambertW
 
 # global parameters
 beta = 0.06
 gamma = 0.05
 
-theoretical_r_limit = 0
-R_0 = beta/gamma
-if R_0 > 1
-    theoretical_r_limit = 1 + 1/R_0 * lambertw(-R_0 * exp(-R_0))
-end
+# computing theoretical r limit
+
+theoretical_r_limit = r_limit(beta, gamma)
 
 
 # running deterministic model
-# time_step = 0.001
-# initial_i = 0.001
-# deterministic_model = DeterministicModel(beta, gamma, initial_i, time_step)
+time_step = 0.001
+initial_i = 0.001
+deterministic_model = DeterministicModel(beta, gamma, initial_i, time_step)
 
-# number_steps = 1000000
+number_steps = 500000
 
-# for step = 1:number_steps
-#     increment_time!(deterministic_model)
-# end
+for step = 1:number_steps
+    increment_time!(deterministic_model)
+end
 
-# steps = []
-# s_values = []
-# i_values = []
-# r_values = []
-# r_limit = []
-# for step = 1:size(deterministic_model.history)[1]
-#     push!(steps, step)
-#     push!(s_values, deterministic_model.history[step].s)
-#     push!(i_values, deterministic_model.history[step].i)
-#     push!(r_values, 1 - deterministic_model.history[step].s - deterministic_model.history[step].i)
-#     push!(r_limit, theoretical_r_limit)
-# end
+steps = []
+s_values = []
+i_values = []
+r_values = []
+r_limit_line = []
+for step = 1:size(deterministic_model.history)[1]
+    if step%10 == 0
+        push!(steps, step*time_step)
+        push!(s_values, deterministic_model.history[step].s)
+        push!(i_values, deterministic_model.history[step].i)
+        push!(r_values, 1 - deterministic_model.history[step].s - deterministic_model.history[step].i)
+        push!(r_limit_line, theoretical_r_limit)
+    end
+end
 
-# plot(steps, [s_values, i_values, r_values, r_limit], title = "deterministic model", label = ["s" "i" "r" "theoretical r limit"])
+figure = plot(steps, [s_values, i_values, r_values, r_limit_line], title = "deterministic model", label = ["s" "i" "r" "theoretical r limit"])
 
 
 # running markovian model
-N = 10000
-initial_I = 10
+N = 100000
+initial_I = 100
 markovian_model = MarkovianModel(beta, gamma, initial_I, N)
 
-number_steps = 100000
+finished = false
 
-for step = 1:number_steps
+while !finished
     increment_step!(markovian_model)
+    global finished = (markovian_model.history[size(markovian_model.history)[1]].I == 0)
 end
 
 dates = []
 S_values = []
 I_values = []
 R_values = []
-R_limit = []
 for step = 1:size(markovian_model.history)[1]
-    push!(dates, markovian_model.history[step].t)
-    push!(S_values, markovian_model.history[step].S)
-    push!(I_values, markovian_model.history[step].I)
-    push!(R_values, N - markovian_model.history[step].S - markovian_model.history[step].I)
-    push!(R_limit, N*theoretical_r_limit)
+    if step%10 == 0
+        push!(dates, markovian_model.history[step].t)
+        push!(S_values, markovian_model.history[step].S/N)
+        push!(I_values, markovian_model.history[step].I/N)
+        push!(R_values, (N - markovian_model.history[step].S - markovian_model.history[step].I)/N)
+    end
 end
 
-plot(dates, [S_values, I_values, R_values, R_limit], title = "Markovian model", label = ["S" "I" "R" "theoretical R limit"])
+plot!(figure, dates, [S_values, I_values, R_values], title = "Deterministic and Markovian models", label = ["S/N" "I/N" "R/N"])
 
+#plot(dates, [S_values, I_values, R_values], title = "Markovian model", label = ["S/N" "I/N" "R/N"])
+
+#savefig(string("figureN_", N, "_initialI_", initial_I, ".png"))
+#savefig("markovian.png")
 
 
